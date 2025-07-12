ccb79456bd512b116f2df1f1d7ceec75a87c1960
Vlad Brylinski <vlad.brylinski@gmail.com>
2025-07-12 21:17:09 +0200

    removed unused prop

--- Model/WorkspaceInstall.cs
+++ Model/WorkspaceInstall.cs
@@ -5,7 +5,6 @@ public class WorkspaceInstall
     public string TeamId { get; set; } = null!;
     public string TeamName { get; set; } = null!;
     public string BotToken { get; set; } = null!;
-    public string BotUserId { get; set; } = null!;
     public string NotifyUserId { get; set; } = null!; // user to notify on timezone change
     public Dictionary<string, string> UserTimezones { get; set; } = new(); // userid -> timezone
 }
